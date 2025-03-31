b1a68444f9826a855176a0c3642028d8d1ce7935
lastfutur <146067264+lastfutur@users.noreply.github.com>
2025-03-31 16:29:59 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -1,6 +1,6 @@
  const session = process.env.SESSION || '';
 const mycode = process.env.CODE || "254";
-const botname = process.env.BOTNAME || 'DREADED';
+const botname = process.env.BOTNAME || 'pantane-md';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
 const database = process.env.DATABASE_URL || '';
